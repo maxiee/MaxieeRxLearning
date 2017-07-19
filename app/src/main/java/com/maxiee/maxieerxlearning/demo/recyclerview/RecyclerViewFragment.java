05b8280041473a51b2af7e0578f458cd44e19cdf
WangRui <maxieewong@gmail.com>
2017-07-19 18:29:03 +0800

    rxrecyclerview

--- app/src/main/java/com/maxiee/maxieerxlearning/demo/recyclerview/RecyclerViewFragment.java
+++ app/src/main/java/com/maxiee/maxieerxlearning/demo/recyclerview/RecyclerViewFragment.java
@@ -5,14 +5,24 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.jakewharton.rxbinding2.support.v7.widget.RecyclerViewChildAttachEvent;
+import com.jakewharton.rxbinding2.support.v7.widget.RecyclerViewChildAttachStateChangeEvent;
+import com.jakewharton.rxbinding2.support.v7.widget.RecyclerViewChildDetachEvent;
+import com.jakewharton.rxbinding2.support.v7.widget.RxRecyclerView;
+import com.jakewharton.rxbinding2.support.v7.widget.RxRecyclerViewAdapter;
+import com.jakewharton.rxbinding2.view.RxView;
 import com.maxiee.maxieerxlearning.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by WangRui on 2017/7/6.
@@ -21,6 +31,9 @@ import butterknife.ButterKnife;
 public class RecyclerViewFragment extends Fragment {
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
+
+    @BindView(R.id.btn_replace_item)
+    Button mButtonReplaceItem;
 
     SimpleAdapter mSimpleAdapter;
 
@@ -33,6 +46,41 @@ public class RecyclerViewFragment extends Fragment {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         mSimpleAdapter = new SimpleAdapter();
         mRecyclerView.setAdapter(mSimpleAdapter);
+
+        RxRecyclerViewAdapter.dataChanges(mSimpleAdapter).subscribe(simpleAdapter -> {
+            Log.d("maxiee", "dataChanges");
+        });
+
+        RxView.clicks(mButtonReplaceItem).subscribe(e -> {
+            mSimpleAdapter.replaceItem(1, "Maxiee");
+            mSimpleAdapter.notifyDataSetChanged();
+        });
+
+        RxRecyclerView
+                .scrollStateChanges(mRecyclerView)
+                .subscribe(scrollState -> {
+                    Log.d("maxiee", "scrollState = " + scrollState);
+                });
+
+        RxRecyclerView.scrollEvents(mRecyclerView)
+                .subscribe(scroll -> {
+                    Log.d("maxiee", "dx = " + scroll.dx() + ", dy = " + scroll.dy());
+                });
+
+        RxRecyclerView.childAttachStateChangeEvents(mRecyclerView).subscribe(event -> {
+            Button button = ((SimpleAdapter.ViewHolder) event.child().getTag()).mButton;
+            CharSequence text = button.getText();
+
+            if (event instanceof RecyclerViewChildAttachEvent) {
+                Log.d("maxiee", "attach " + text);
+                return;
+            }
+
+            if (event instanceof RecyclerViewChildDetachEvent) {
+                Log.d("maxiee", "detach " + text);
+                return;
+            }
+        });
 
         return v;
     }
--- app/src/main/java/com/maxiee/maxieerxlearning/demo/recyclerview/SimpleAdapter.java
+++ app/src/main/java/com/maxiee/maxieerxlearning/demo/recyclerview/SimpleAdapter.java
@@ -4,11 +4,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by WangRui on 2017/7/6.
  */
 
 public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder> {
+
+    private List<String> mData;
+
+    public SimpleAdapter() {
+        mData = new LinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            mData.add(String.valueOf(i));
+        }
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -21,12 +33,21 @@ public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mButton.setText(String.valueOf(position));
+        holder.mButton.setText(mData.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 100;
+        return mData.size();
+    }
+
+    public boolean replaceItem(int index, String newString) {
+        if (index < 0 || index >= getItemCount()) {
+            return false;
+        }
+
+        mData.set(index, newString);
+        return true;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -35,6 +56,7 @@ public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder
         public ViewHolder(Button button) {
             super(button);
             mButton = button;
+            mButton.setTag(this);
         }
     }
 }
